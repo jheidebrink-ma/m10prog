44854ba7f74e9dd45f1bd1209018da2053107411
Jasper Heidebrink <j.heidebrink@ma-web.nl>
2023-11-22 16:21:00 +0100

    Classes setup

--- public/index.php
+++ public/index.php
@@ -1,3 +1,41 @@
 <?php
 
-echo 'M10 PROG';
+
+class Auto
+{
+
+    // Properties
+    private string $name;
+    private string $color = 'rood';
+
+    /**
+     * Hier stel ik de naam in
+     *
+     * @param string $name
+     * @return void
+     */
+    function set_name(string $name = 'mini'): string
+    {
+        $this->name = $name;
+        return $this->name;
+    }
+
+    /**
+     * Haal de naam op
+     *
+     * @return string
+     */
+    function get_name(): string
+    {
+        return $this->name;
+    }
+}
+
+
+$mijnAuto = new Auto();
+$mijnAuto->set_name('herby');
+
+$anderAuto = new Auto();
+$anderAuto->set_name('henk');
+
+echo $anderAuto->get_name();
