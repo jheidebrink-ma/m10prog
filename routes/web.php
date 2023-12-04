c3e8cdd0703c986d91b2e3855e246248d5b9d6fd
Jasper Heidebrink <j.heidebrink@ma-web.nl>
2023-12-04 21:08:09 +0100

    lesson 3

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/about', [ \App\Http\Controllers\AboutController::class, 'index'])->name('about');
