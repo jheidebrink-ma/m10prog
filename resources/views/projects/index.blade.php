f10ca22532cc4b585aa74982d49f79d82b330c9f
jheidebrink-ma <143648488+jheidebrink-ma@users.noreply.github.com>
2024-01-13 13:02:21 +0100

    better project overview

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,15 +2,33 @@
 
 @section('content')
     Hier komen mijn projecten:
-    <ul class="w-full">
+    <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
         @foreach( $projects as $project)
-            <li class="w-full">
-                <h2>{{$project->title}}</h2>
+            <div
+                class="max-w-sm p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="{{route('project.show', $project)}}">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">{{$project->title}}</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 italic">
+                    {{$project->intro}}
+                </p>
                 <a href="{{route('project.show', $project)}}"
-                   class="bg-green hover:bg-green text-white font-bold py-2 px-4 rounded">Bekijk project</a>
-            </li>
+                   class="bg-green focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                    Bekijk
+                </a>
+                @if (Route::has('login'))
+                    <a href="{{route('project.show', $project)}}"
+                       class="bg-yellow me-2 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+                        Wijzig
+                    </a>
+                    <a href="{{route('project.show', $project)}}"
+                       class="bg-orange focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                    Verwijder
+                    </a>
+                @endif
+            </div>
         @endforeach
-    </ul>
+    </div>
 
 @endsection
 
